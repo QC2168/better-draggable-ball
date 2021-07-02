2916aad6113f8200f14d1923b59ca76f27025386
QIANCHENG2125 <huangxinhong2125@gmail.com>
2021-07-02 22:23:54 +0800

    多类型打包

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,49 +1,58 @@
 import * as path from 'path';
 import { Configuration as WebpackConfiguration } from 'webpack';
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import TerserPlugin from 'terser-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
-
-// const net = require('net');
-// // 获取端口
-// function getPort(port: number = 9000): Promise<number> {
-//   // 创建服务并监听该端口
-//   const server = net.createServer().listen(port);
-//   return new Promise<number>((resolve, reject) => {
-//     server.on('listening', () => {
-//       server.close();
-//       resolve(port);
-//     });
-
-//     server.on('error', (err:any) => {
-//       if (err.code === 'EADDRINUSE') {
-//         resolve(getPort(port + 1));// 注意这句，如占用端口号+1
-//       } else {
-//         reject(err);
-//       }
-//     });
-//   });
-// }
 
 interface Configuration extends WebpackConfiguration {
   devServer ?: WebpackDevServerConfiguration;
 }
-const config: Configuration[] = [{
+const outputConfig: Configuration[] = [
+  {
+    output: {
+      path: path.resolve(__dirname, 'dist/commonjs'),
+      filename: '[name].js',
+      clean: true,
+      library: {
+        name: 'betterDraggableBall',
+        type: 'commonjs',
+        export: 'default',
+      },
+    },
+  },
+  {
+    output: {
+      path: path.resolve(__dirname, 'dist/umd'),
+      filename: '[name].js',
+      clean: true,
+      library: {
+        name: 'betterDraggableBall',
+        type: 'umd',
+        export: 'default',
+      },
+    },
+  },
+  {
+    output: {
+      path: path.resolve(__dirname, 'dist/module'),
+      filename: '[name].js',
+      clean: true,
+      module: true,
+      library: {
+        type: 'module',
+      },
+    },
+    experiments: {
+      outputModule: true,
+    },
+  },
+];
+const baseConfig: Configuration = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   devtool: process.env.NODE_ENV === 'production' ? 'cheap-module-source-map' : 'cheap-module-source-map',
   entry: {
-    Drag: './src/Drag.ts',
-    'Drag.min': './src/Drag.ts',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    module: true,
-    library: {
-      type: 'module',
-    },
+    'better-draggable-ball': './src/Drag.ts',
+    'better-draggable-ball.min': './src/Drag.ts', // 压缩版本
   },
   experiments: {
     outputModule: true,
@@ -93,10 +102,6 @@ const config: Configuration[] = [{
     extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'drag',
-      template: './src/index.html',
-    }),
     new ESLintPlugin({
       extensions: ['js', 'ts'],
       exclude: '/node_modules/',
@@ -118,7 +123,11 @@ const config: Configuration[] = [{
       }),
     ],
   },
-},
-];
+};
 
+const config: Configuration[] & Configuration | [] | any = [];
+for (let i: number = 0; i < outputConfig.length; i += 1) {
+  config[i] = Object.assign(outputConfig[i], baseConfig);
+}
+config.parallelism = outputConfig.length;
 export default config;
